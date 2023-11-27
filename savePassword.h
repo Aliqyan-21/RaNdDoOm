65f984d220a1c6bc732148812049ada667111e6d
aliqyan-21 <aliqyanabid21@gmail.com>
2023-11-27 19:39:17 +0530

    updated it to add random.h function and to save encryted password now

--- savePassword.h
+++ savePassword.h
@@ -1,12 +1,14 @@
 #ifndef SAVEPASSWORD_H
-# define SAVEPASSWORD_H
+#define SAVEPASSWORD_H
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <random>
+#include <vector>
+#include "random.h"
 
-void passwordSave(string password)
-{
+void passwordSave(vector<int>enc){
 	fstream pswrdFile;
 	string pswrdKey;
 
@@ -15,9 +17,16 @@ void passwordSave(string password)
 
 	pswrdFile.open("passwords.txt", ios::app);
 
+  vector <char> sym = {'@', '#', '$'};
+
+  int ran;
 	if(pswrdFile.is_open())
 	{
-		pswrdFile << pswrdKey << " : " << password << endl;
+    pswrdFile << pswrdKey << " : ";
+    for(int val : enc){
+      ran = makeRandom(0, 2);
+		  pswrdFile << val << sym[ran];
+    }
 		pswrdFile.close();
 	}
 }
