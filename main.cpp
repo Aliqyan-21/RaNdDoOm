e287fdf6fd90e52c336ecab57189d78cc38f6afd
aliqyan-21 <aliqyanabid21@gmail.com>
2023-11-28 18:19:11 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,67 @@
 #include <iostream>
+#include <string>
 #include "generator.h"
 #include "savePassword.h"
 #include "encrypt.h"
+#include "decrypt.h"
 using namespace std;
 
 int main()
 {
-	cout << "Enter length of password(should be 12 or more for strongness): "; 
-	int n;
-	cin >> n;
+	while(1){
+    int option;
+    cout << "\n1. Generate Password\n2. Decrypt Saved Password\n0. Exit\n\nEnter Your Choice[1/2/0]: ";
+    cin >> option;
+    if(option == 1){
+	    cout << "Enter length of password(should be 12 or more for strongness): "; 
+	    int n;
+	    cin >> n;
 
-	string password = passwordGenerator(n);
+	    string password = passwordGenerator(n);
 
-	cout << "Password generated is: " << password << endl;
+	    cout << "Password generated is: " << password << endl;
 
-	char choice;
+	    char choice;
+		  cout << "Do you want to save password in a passwords.txt file[y/n]: ";
+		  cin >> choice;
 
-	while(1)
-	{
-		cout << "Do you want to save password in a passwords.txt file[y/n]: ";
-		cin >> choice;
+		  if(choice == 'Y' || choice == 'N') choice += 32;
 
-		if(choice == 'Y' || choice == 'N') choice += 32;
+		  if(choice == 'y'){
+        vector <int> enc = encrypt(password);
+		  	passwordSave(enc);
+		  	continue;
+		  }
 
-		if(choice == 'y')
-		{
-      vector <int> enc = encrypt(password);
-			passwordSave(enc);
-			break;
-		}
+		  else if(choice == 'n'){
+		  	cout << "Thank you, copy and paste the password to use it\n"; 
+		  	continue;
+		  }
 
-		else if(choice == 'n')
-		{
-			cout << "Thank you, copy and paste the password to use it\n"; 
-			break;
-		}
+		  else{
+		  	cout << "Enter a valid command [y/n]\n";
+		  }
+    }
 
-		else 
-		{
-			cout << "Enter a valid command [y/n]\n";
-		}
+    if(option == 2){
+      string dec_key;
+      cout << "Enter key to decrypt: ";
+      cin >> dec_key;
+      dec_key += " ";
+      string s = decrypt(dec_key);
+      if(s == "Err"){
+        cout << "Key not found -> Error-404!\n";
+      }
+      else{
+        cout << "Your password for " << dec_key << " is : " << s << endl;
+      }
+      continue;
+    }
+
+    if(option == 0){
+      cout << "Exiting..." << endl;
+      break;
+    }
 	}
-	
 	return 0;
 }
